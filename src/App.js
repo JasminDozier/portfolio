a917903049315c70eec73fad71b91127b6a7b1d6
Jasmin Dozier <jasmindozier93@gmail.com>
2019-09-11 22:14:45 -0400

    project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router";
+import { BrowserRouter, Link } from "react-router-dom";
+import "bulma/css/bulma.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <div>
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/contact">Contact</Link>
+              <h1>Home Page</h1>
+            </div>
+          )}
+        />
+        <Route
+          path="/about"
+          render={() => (
+            <div>
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/contact">Contact</Link>
+              <h1>About Page</h1>
+            </div>
+          )}
+        />
+        <Route
+          path="/contact"
+          render={() => (
+            <div>
+              <Link to="/">Home</Link>
+              <Link to="/about">About</Link>
+              <Link to="/contact">Contact</Link>
+              <h1>Contact Page</h1>
+            </div>
+          )}
+        />
+        {/* when none of the above match, <NoMatch> will be rendered */}
+        <Route render={() => <div>Page not found</div>} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
